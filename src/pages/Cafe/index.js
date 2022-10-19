5615792051ccddc8d30499970489a7ba289cb150
Davi Oliveira Brito <davioliveirabrito07@gmail.com>
2022-10-19 15:46:09 -0300

    testes

--- src/pages/Cafe/index.js
+++ src/pages/Cafe/index.js
@@ -5,28 +5,18 @@ export default function CalcularCafé(){
     const [n, setN] = useState(0);
     const [capacidade, setCapacidade] = useState(0);
     const [d, setD] = useState(0);
-    const [resposta, setResposta] = useState(0
-        );
+    const [resposta, setResposta] = useState(0);
 
     function click() {
-        let convesao = d * n;
-        let coffeNecessario = convesao / 1000;
-        
+        let coffeNecessario  = (d * n) / 1000;
+        let minnimo = capacidade;
 
         while(coffeNecessario > capacidade){
-            capacidade+=capacidade
-            
-            
-            if(coffeNecessario < capacidade){
-                capacidade=capacidade;
-                setResposta(capacidade);
-            }
-            
+            coffeNecessario = minnimo+=minnimo;
+            setResposta(coffeNecessario);
+            console.log(coffeNecessario)
         }
-        setResposta(capacidade);
-
-        
-
+        setResposta(coffeNecessario);
     }
 
 
@@ -36,7 +26,7 @@ export default function CalcularCafé(){
               <input type="text" placeholder='alunos' value={n}  onChange={ e => setN(Number(e.target.value))} />
               <input type="text" placeholder='Capacidade' value={capacidade}  onChange={ e => setCapacidade(Number(e.target.value))} />
               <input type="text" placeholder='ML' value={d}  onChange={ e => setD(Number(e.target.value))} />
-              <button onClick={click} >Calcular</button>
+              <button onClick={() => click()} >Calcular</button>
               <p>{resposta}</p>
         </main>
 
